7226a60cf7b63dda052f0872606f6c692d3ef8d9
vishalicious213 <vishalicious213@gmail.com>
2019-11-09 02:43:31 -0500

    added router to App.js. added welcome page. made routes to welcome page and character page.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
+import { Route } from "react-router-dom";
 import Header from "./components/Header.js";
 import CharacterList from "./components/CharacterList";
+import WelcomePage from "./components/WelcomePage.js";
 
 
 export default function App() {
   return (
     <main>
       <Header />
-      <CharacterList />
+        <Route exact path="/" component={WelcomePage} />
+        <Route path="/characters" component={CharacterList} />
     </main>
   );
 }
