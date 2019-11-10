2210719aa14e82d41951c8d26c194cdc6c94d02e
vishalicious213 <vishalicious213@gmail.com>
2019-11-10 04:14:56 -0500

    upgraded search bar and button from css to styled components

--- src/components/Header.js
+++ src/components/Header.js
@@ -33,7 +33,6 @@ const Nav = styled.nav`
 `;
 
 const Top = styled.header`
-
   display: flex;
   align-items: center;
   justify-content: space-between;
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,4 +1,40 @@
 import React, { useState } from "react";
+import styled from "styled-components";
+
+const Input = styled.input`
+  font-size: 2rem;
+  border: 3px solid black;
+  border-radius: .5rem;
+  color: darkkhaki;
+  background: cadetblue;
+  margin: 1px;
+  text-shadow:  
+    3px 3px 0 #000,
+    -1px -1px 0 #000,  
+    1px -1px 0 #000,
+    -1px 1px 0 #000,
+    1px 1px 0 #000;`
+
+const Button = styled.button`
+  font-size: 2rem;
+  width: 10rem;
+  color: cadetblue;
+  font-weight: bold;
+  background: darkkhaki;
+  border: 3px solid black;
+  border-radius: .5rem;
+  margin: 1px;
+  text-shadow:  
+    3px 3px 0 #000,
+    -1px -1px 0 #000,  
+    1px -1px 0 #000,
+    -1px 1px 0 #000,
+    1px 1px 0 #000;
+  :hover {
+    color: darkkhaki;
+    background: cadetblue;
+  }
+}`
 
 export default function SearchForm() {
  
@@ -6,8 +42,8 @@ export default function SearchForm() {
     <section className="search-form">
      {/* Add a search form here */}
      <form>
-       <input type='text' name='search' placeholder='Enter character name' />
-       <button>Search</button>
+       <Input type='text' name='search' placeholder='Enter character name' />
+       <Button>Search</Button>
      </form>
     </section>
   );
